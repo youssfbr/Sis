a083bc15f84583cf26225277bfe0f23bb1d1756d
alisson youssf <youssfbr@gmail.com>
2020-08-19 08:32:45 -0300

    Primeira migracao no BD

---
+++ SisTest/Data/SisTestContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace SisTest.Models
+{
+    public class SisTestContext : DbContext
+    {
+        public SisTestContext (DbContextOptions<SisTestContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<SisTest.Models.Cliente> Cliente { get; set; }
+    }
+}
---
+++ SisTest/Migrations/20200819111331_Primeira_migracao_cliente.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SisTest.Migrations
+{
+    public partial class Primeira_migracao_cliente : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Cliente",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    Nome = table.Column<string>(nullable: true),
+                    Sexo = table.Column<string>(nullable: true),
+                    Telefone = table.Column<string>(nullable: true),
+                    Email = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Cliente", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Cliente");
+        }
+    }
+}
