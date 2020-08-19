580d474395d844814205d2a4bbd16c1de9650d11
alisson youssf <youssfbr@gmail.com>
2020-08-19 08:29:49 -0300

    Conectando ao MySQL

--- SisTest/Startup.cs
+++ SisTest/Startup.cs
@@ -9,6 +9,8 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+using SisTest.Models;
 
 namespace SisTest
 {
@@ -33,6 +35,10 @@ namespace SisTest
 
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            services.AddDbContext<SisTestContext>(options =>
+                    options.UseMySql(Configuration.GetConnectionString("SisTestContext"), builder => 
+                    builder.MigrationsAssembly("SisTest")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
